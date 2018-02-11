c6d6e81703d1076afc9e5b25cc532fb873516c14
navib <ivan.baena.dev@gmail.com>
2018-02-10 21:43:02 -0500

    react redux react-router api posts

---
+++ src/components/posts_new.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import  { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {createPost} from '../actions';
+
+class PostsNew extends Component {
+
+  renderField(field) {
+    const { meta: {touched, error} } = field;
+    const className= `form-group ${touched && error ? 'has-danger': ''}`
+
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type={field.type}
+          {...field.input}
+        />
+        <div className="text-help">
+          {field.meta.touched ? field.meta.error: ''}
+        </div>
+
+      </div>
+    )
+  }
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <div>
+        <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+          <Field
+            name="title"
+            component={this.renderField}
+            label="Title"
+            type="text"
+          />
+          <Field
+            name="categories"
+            component={this.renderField}
+            label="Categories"
+            type="text"
+          />
+          <Field
+            name="content"
+            component={this.renderField}
+            label="Post Content"
+            type="text"
+          />
+          <button type="submit" className="btn btn-primary">Submit</button>
+          <Link className="btn btn-danger" to="/">Cancel</Link>
+        </form>
+      </div>
+    )
+  }
+}
+
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = "Enter a title"
+  }
+  if(!values.categories) {
+    errors.categories = "Enter some category"
+  }
+  if(!values.content) {
+    errors.content = "Enter some content please"
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(
+  connect(null, { createPost })(PostsNew)
+);
---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,21 @@
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
+import _ from 'lodash';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload)
+      break;
+    case FETCH_POST:
+      const post = action.payload.data;
+
+      return { ...state, [action.payload.data.id]: action.payload.data }
+      break;
+    case FETCH_POSTS:
+      return _.mapKeys(action.payload.data, 'id');
+      break;
+    default:
+    return state;
+
+  }
+}
